f35305157f52d7177a16ec1163d7f03ada014872
Connor McCue <connor.mccue@comcast.net>
2022-09-26 18:07:42 -0700

    working product should have commited more

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import BookList from './components/book/BookList';
 import { Route, Switch } from 'react-router-dom';
-// import Book from './components/book/Book';
-// import BookDetail from './components/book/BookDetail';
-import Author from './components/author/Author';
+import Book from './components/book/Book';
+import BookDetail from './components/book/BookDetail';
 import NotFound from './components/notfound/NotFound';
 import Home from './components/home/Home';
 import './App.css';
@@ -13,10 +12,9 @@ function App() {
     <>
       <h1>Library App</h1>
       <Switch>
-        <Route path="/" component={Home} />
-        {/* <Route path="/books/:id" component={BookDetail} /> */}
+        <Route path="/books/:id" component={BookDetail} />
         <Route path="/books" component={BookList} />
-        <Route path="/authors" component={Author} />
+        <Route path="/" component={Home} />
         <Route path="*" component={NotFound} />
       </Switch>
     </>
